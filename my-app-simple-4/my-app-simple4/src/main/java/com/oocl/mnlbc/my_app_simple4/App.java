1eb5698847ffc57a160de16517778b88d6dc9349
William <william.kalingasan@oocl.com>
2016-06-13 17:54:54 +0800

    Exercise demo

---
+++ my-app-simple-4/my-app-simple4/src/main/java/com/oocl/mnlbc/my_app_simple4/App.java
@@ -0,0 +1,13 @@
+package com.oocl.mnlbc.my_app_simple4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
